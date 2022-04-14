6eaa8476bca7fa08cea99cb3e6a4d07b8de42fc6
U-and-Me <yunasun0920@gmail.com>
2022-04-14 14:55:58 +0900

    AOP 실습

---
+++ AOP/src/main/java/kr/hs/study/beans/TestBean1.java
@@ -0,0 +1,13 @@
+package kr.hs.study.beans;
+
+// target 클래스
+// joinpoint : method1, method2
+public class TestBean1 {
+	public void method1() {
+		System.out.println("TestBean1의 method1");
+	}
+	
+	public void method2() {
+		System.out.println("TestBena1의 method2");
+	}
+}
