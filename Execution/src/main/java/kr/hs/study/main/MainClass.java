eecbdc966cf973a10cc4df46ebe3635f3bfd72ab
U-and-Me <yunasun0920@gmail.com>
2022-04-18 11:17:19 +0900

    Execution 실습

--- Execution/src/main/java/kr/hs/study/main/MainClass.java
+++ Execution/src/main/java/kr/hs/study/main/MainClass.java
@@ -2,6 +2,8 @@ package kr.hs.study.main;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import kr.hs.study.beans.TestBean1;
+
 public class MainClass {
 
 	public static void main(String[] args) {
@@ -9,7 +11,13 @@ public class MainClass {
 
 		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("kr/hs/study/config/config.xml");
 		
+		TestBean1 obj1 = ctx.getBean("t1", TestBean1.class);
 		
+		obj1.method1();
+		obj1.method2();
+		obj1.method1(100);
+		obj1.method1("spring");
+		obj1.method1(100, 200);
 		
 		ctx.close();
 		
