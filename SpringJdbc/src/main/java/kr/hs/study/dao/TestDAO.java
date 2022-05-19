ff3be54369fd886e485f5c4dcd0acadcf3482485
U-and-Me <yunasun0920@gmail.com>
2022-05-19 15:03:56 +0900

    oracle - update, delete 실습

--- SpringJdbc/src/main/java/kr/hs/study/dao/TestDAO.java
+++ SpringJdbc/src/main/java/kr/hs/study/dao/TestDAO.java
@@ -17,7 +17,20 @@ public class TestDAO {
 	// 1. insert
 	public void insert_data(TestBean bean) {
 		// container에 있는 TestBean의 객체가 bean에 자동주입됨
-		String sql = "insert into test values(?, ?)";
+		String sql = "insert into testJdbc values(?, ?)";
 		db.update(sql, bean.getData1(), bean.getData2());
 	}
+	
+	// 2. update
+	public void update_data(TestBean bean) {
+		String sql = "update testJdbc set data2=? where data1=?";
+		db.update(sql, bean.getData2(), bean.getData1());
+	}
+	
+	// 3. delete
+	public void delete_data(int data1) {
+		String sql = "delete from testJdbc where data1=?";
+		db.update(sql, data1);
+	}
+	
 }
--- SpringJdbc/src/main/java/kr/hs/study/main/MainClass.java
+++ SpringJdbc/src/main/java/kr/hs/study/main/MainClass.java
@@ -16,21 +16,40 @@ public class MainClass {
 		// dao 가져오기
 		TestDAO dao = ctx.getBean(TestDAO.class);
 		
+		// insert
+	/*	
 		// TestBean 객체 t1 만들어서 100, spring 넣기
 		TestBean t1 = ctx.getBean(TestBean.class);
-		t1.setData1(100);
+		t1.setData1(10);
 		t1.setData2("spring");
 		
 		// dao의 insert_data 호출
 		dao.insert_data(t1);
 		
 		TestBean t2 = ctx.getBean(TestBean.class);
-		t2.setData1(200);
+		t2.setData1(20);
 		t2.setData2("spring2");
 		
 		dao.insert_data(t2);
 		
 		System.out.println("Inserted!!");
+	*/	
+		
+		// update
+		/*
+		TestBean t3 = ctx.getBean(TestBean.class);
+		t3.setData1(10);
+		t3.setData2("spring100");
+		
+		dao.update_data(t3);
+		
+		System.out.println("Updated!!");
+		*/
+		
+		// delete
+		dao.delete_data(10);
+		
+		System.out.println("Deleted!!");
 		
 		ctx.close();
 
